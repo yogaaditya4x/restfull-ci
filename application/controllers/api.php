91a59f2b9de7d50a58e57593774e6b8ed17438c2
Yoga Aditya <50347730+yogaaditya4x@users.noreply.github.com>
2020-10-09 15:03:56 +0700

    Update api.php

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -32,10 +32,87 @@ class Api extends CI_Controller
 				'nim'	=> $this->input->post('nim')
 			);
 
-			$this->api_model->update_api($this->input->post('nim'), $data);
+			$this->api_model->insert_api($data);
+
+			$array = array(
+				'success'		=> true 
+			);
+		}
+		else
+		{
+			$array = array(
+				'error'			=> true,
+				'nama_error'	=> form_error('nama'),
+				'nim_error'		=> form_error('nim')
+				);
+		}
+		echo json_encode($array);
+	}
+
+	function fetch_single()
+	{
+		if($this->input->post('nim'))
+		{
+			//echo "string";
+			$data = $this->api_model->fetch_single_user($this->input->post('nim'));
+
+			foreach ($data as $row) 
+			{
+				$output['nama'] = $row['nama'];
+				$output['nim'] = $row['nim'];
+			}
+			echo json_encode($output);
 		}
 	}
+
+	function update()
+	{
+		$this->form_validation->set_rules('nama','Nama Lengkap','required');
+		$this->form_validation->set_rules('nim','NIM','required');
+		if($this->form_validation->run())
+		{
+			$data = array(
+				'nama'		=> $this->input->post('nama'),
+				'nim'		=> $this->input->post('nim')
+			);
+
+			$this->api_model->update_api($this->input->post('nim'), $data);
+
+			$array = array(
+				'success'		=> true 
+			);
+		}
+		else
+		{
+			$array = array(
+				'error'			=> true,
+				'nama_error'	=> form_error('nama'),
+				'nim_error'		=> form_error('nim')
+			);	
+		}
+		echo json_encode($array);
+	}
+
+	function delete()
+	{
+		if($this->input->post('nim'))
+		{
+			$array = array(
+				'success'		=> true 
+			);
+		}
+		else
+		{
+			$array = array(
+				'error'			=> true 
+			);
+		}
+		echo json_encode($array);
+	}
+	
 }
+
+
 
 
  ?>
